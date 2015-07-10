1ba899412aee29775772a8cb93a5e514eef9a918
1o0ko <wojciech.stokowiec@gmail.com>
2015-07-10 16:31:08 +0200

    - renamed variables

--- src/main/java/TT4J/example/PersistentMap.java
+++ src/main/java/TT4J/example/PersistentMap.java
@@ -12,23 +12,23 @@ public class PersistentMap {
     private static final String DB_NAME = "pers.db";
 
     public static void init() {
-        Connection c = null;
-        Statement stmt = null;
+        Connection connection;
+        Statement stmt ;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
-            c.setAutoCommit(false);
+            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
+            connection.setAutoCommit(false);
             System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
+            stmt = connection.createStatement();
             String sql = "CREATE TABLE PERSISTANCE " +
                     "(GUID TEXT PRIMARY KEY     NOT NULL," +
                     " VALUE          TEXT    NOT NULL)";
 
             stmt.executeUpdate(sql);
             stmt.close();
-            c.commit();
-            c.close();
+            connection.commit();
+            connection.close();
             System.out.println("Database created successfully");
         } catch ( Exception e ) {
             System.out.println("Database already exists");
@@ -37,21 +37,21 @@ public class PersistentMap {
     }
 
     public static void put(String guid, String value) throws Exception {
-        Connection c = null;
-        Statement stmt = null;
+        Connection connection;
+        Statement stmt;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
-            c.setAutoCommit(false);
+            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
+            connection.setAutoCommit(false);
             System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
+            stmt = connection.createStatement();
             String sql = String.format("INSERT INTO PERSISTANCE (GUID, VALUE) VALUES ('%s','%s')",guid,value);
 
             stmt.executeUpdate(sql);
             stmt.close();
-            c.commit();
-            c.close();
+            connection.commit();
+            connection.close();
             System.out.println("Record added successfully");
         } catch ( Exception e ) {
             System.out.println("Error adding record");
@@ -61,24 +61,24 @@ public class PersistentMap {
     }
 
     public static String get(String guid) {
-        Connection c = null;
-        Statement stmt = null;
+        Connection connection;
+        Statement stmt;
         String result = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
-            c.setAutoCommit(false);
+            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s.db",DB_NAME));
+            connection.setAutoCommit(false);
             System.out.println("Opened database successfully");
 
-            stmt = c.createStatement();
+            stmt = connection.createStatement();
             String sql = String.format("SELECT VALUE FROM PERSISTANCE WHERE GUID = '%s'", guid);
             ResultSet rs = stmt.executeQuery(sql);
             while ( rs.next() ) {
                 result = rs.getString("VALUE");
             }
             stmt.close();
-            c.commit();
-            c.close();
+            connection.commit();
+            connection.close();
             System.out.println("Queried successfully");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
